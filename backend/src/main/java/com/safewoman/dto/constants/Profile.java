45a449a0d9e5dc4ce478ed3b10d11648c4693848
Yasmin Wichinievski <yas.desenv@gmail.com>
2022-06-28 20:41:46 -0300

    ending Authorization JWT Filter

---
+++ backend/src/main/java/com/safewoman/dto/constants/Profile.java
@@ -0,0 +1,37 @@
+package com.safewoman.dto;
+
+public enum Profile {
+
+    ADMIN(0, "ROLE_ADMIN"),
+    USER(1, "ROLE_USER");
+
+    private Integer code;
+    private String description;
+
+    private Profile(Integer code, String description){
+        this.code = code;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public static Profile toEnum(Integer cod) {
+        if(cod == null) {
+            return null;
+        }
+
+        for(Profile profile : Profile.values()) {
+            if(cod.equals(profile.getCode())) {
+                return profile;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid Profile");
+    }
+
+}
